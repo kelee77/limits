11011197191ced5d49b44cdd14dac97044b4af57
sunhongyin <sunhongyin@sunhongyindemacbook-pro.local>
2016-03-07 17:32:26 +0800

    1

--- limits/main.cpp
+++ limits/main.cpp
@@ -7,9 +7,18 @@
 //
 
 #include <iostream>
+#include <climits>
 
 int main(int argc, const char * argv[])
 {
-
+    using namespace std;
+    
+    int n_int = INTMAX_MAX;
+    short n_short = SHRT_MAX;
+    long n_long = LONG_MAX;
+    long long n_llong = LLONG_MAX;
+    
+    
+    
     return 0;
 }
